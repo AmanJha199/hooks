c8f2311ba4891878aa0f0714d2219fc2d0ed7795
AmanJha199 <43825635+AmanJha199@users.noreply.github.com>
2021-07-04 20:39:09 +0530

    hooks intro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+// import Us from './UseState/Us';
+// import Ue2 from './useEffect/Ue2';
+// import Ue3 from './useEffect/Ue3';
+import Uewc from './useEffect/cleanUp/Uewc';
 
 function App() {
   return (
-    <h1>Hello</h1>
+<>
+{/* <h1>Heelo</h1> */}
+{/* <Us></Us> */}
+{/* <Ue2/> */}
+{/* <Ue3/> */}
+<Uewc></Uewc>
+
+
+</>
   );
 }
 
